aeea1dc4451177ff93e873206ea545787cf5b36e
Stef Janssen <steffos.janssen@gmail.com>
2014-10-23 20:57:57 +0200

    core data gefixt

--- ReinforcementTeacher/ReinforcementTeacher/ViewController.swift
+++ ReinforcementTeacher/ReinforcementTeacher/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
+    lazy var managedObjectContext : NSManagedObjectContext? = {
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        if let managedObjectContext = appDelegate.managedObjectContext {
+            return managedObjectContext
+        }
+        else {
+            return nil
+        }
+        }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let user = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: self.managedObjectContext!) as User
+        user.name = "een naam"
+        user.ipaddress = "de ip address"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
